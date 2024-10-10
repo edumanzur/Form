72b3fafdfe3f28526f6c5af00377c73ef3216fd5
Eduardo Manzur Finotti Seabra <eduardo.manzur@dinfor.uniceub>
2024-10-10 17:49:40 -0300

    Melhora no cod

--- src/componentes/Box/index.jsx
+++ src/componentes/Box/index.jsx
@@ -1,15 +1,16 @@
 import './style.css'
 export default function Box() {
     return (
-        <>
-            <div>
+        <main>
+            
                 <form action="">
                     <h3>Login</h3>
                     <input type="text" placeholder='Usuário' required/><br />
                     <input type="password" placeholder='Senha' required/><br />
                     <button>Enviar</button>
                 </form>
-            </div>
-        </>
+            
+        </main>
+            
     )
 }
